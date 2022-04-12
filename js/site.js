c1584826aac4d46c3bfa410b08b0a5a5963c215a
Charles Hall <webdevcharles@gmail.com>
2022-04-12 11:06:00 -0400

    Additional JS functions

--- js/site.js
+++ js/site.js
@@ -42,6 +42,51 @@ function fizzBuzzA(fizz, buzz) {
     return fbValues;
 }
 
+function fizzBuzzB(fizz, buzz) {
+    let fbValues = [];
+
+    let isFizz = false;
+    let isBuzz = false;
+
+    for (let i = 1; i < 101; i++) {
+
+        isFizz = (i % fizz == 0);
+        isBuzz = (i % buzz == 0);
+
+        switch(true) {
+            case isFizz && isBuzz: {
+                fbValues.push('FizzBuzz');
+                break;
+            } 
+            case isFizz: {
+                fbValues.push('Fizz');
+                break;
+            } 
+            case isBuzz: {
+                fbValues.push('Buzz');
+                break;
+            } 
+            default: {
+                fbValues.push(i);
+                break;
+            }
+        }
+    }
+
+    return fbValues;
+}
+
+function fizzBuzzC(fizz, buzz) {
+    let fbValues = [];
+    
+    for (let i = 1; i < 101; i++) {
+        let value = ((i % fizz == 0 ? 'Fizz' : '') + (i % buzz == 0 ? 'Buzz' : '') || i )
+        fbValues.push(value);
+    }
+    return fbValues;
+}
+
+
 function displayValues(fbValues) {
 
     let element = document.getElementById("results");
@@ -60,7 +105,7 @@ function displayValues(fbValues) {
 
         } else if (fbValues[index] == 'Buzz') {
             item.classList.add('buzz');
-            
+
         } else if (fbValues[index] == 'FizzBuzz') {
             item.classList.add('fizzBuzz');
         }
